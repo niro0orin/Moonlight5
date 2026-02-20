7790bd9c5cdd3de97d2e5d004926c7c3e24fe2c1
niro0orin <niro0orin@gmail.com>
2026-02-20 10:48:48 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,7 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
-      <body>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
